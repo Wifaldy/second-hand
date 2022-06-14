4a47f7740e2f766ab9f3198d4824e280102a23d0
Andri Wifaldy <andriwifaldy@gmail.com>
2022-06-14 20:59:48 +0700

    fix: bug on models/index

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const userRoutes = require("./routes/user.route");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
