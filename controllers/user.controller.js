c4fb9e1ca4421b340c2e6e32b3545e6c71ac8673
Khoirul Anam <khairulanamstud@gmail.com>
2022-06-14 21:41:15 +0700

    add register user feature

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,26 +1,27 @@
-const { user } = require('../models')
-class UserController{
-
-    static async registerUser(req, res, next){
-        try {
-            const { name, email, password} = req.body;
-            if (!name || !email || !password ) throw {
-                status: 400,
-                message: 'Name, Email, or Password should not be empty'
-            }
-            await user.create({
-                name,
-                email,
-                password,
-                createdAt: new Date,
-                updatedAt: new Date(),
-            })
-        } catch (error) {
-            next(error);
-        }
+const { user } = require("../models");
+class UserController {
+  static async registerUser(req, res, next) {
+    try {
+      const { name, email, password } = req.body;
+      if (!name || !email || !password)
+        throw {
+          status: 400,
+          message: "Name, Email, or Password should not be empty",
+        };
+      await user.create({
+        name,
+        email,
+        password,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+      res.status(201).json({
+        message: 'Success add new user'
+      })
+    } catch (error) {
+      next(error);
     }
-
-
+  }
 }
 
 module.exports = UserController;
