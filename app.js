0620d1ebcfb51e7de4f2cd2ab694cd6f63997604
Andri Wifaldy <andriwifaldy@gmail.com>
2022-06-20 19:36:29 +0700

    add: cors

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const errorHandler = require("./error_handler");
 const router = require("./routes");
 const swaggerUI = require("swagger-ui-express");
 const swaggerDocument = require("./SecondHandSwagger.json");
+const cors = require("cors");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(cors());
 app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use(router);
 app.use(errorHandler);
